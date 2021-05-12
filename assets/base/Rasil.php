c23140d0000c489ed4443b3845e1d5e7ca9f3224
Andrei0903 <krolikakanalturba@mail.ru>
2021-05-12 15:22:33 +0500

    a

---
+++ assets/base/Rasil.php
@@ -0,0 +1,20 @@
+<section id="contact" class="prefooter wow fadeInUp" data-wow-delay="300ms">
+            <!-- change the image in style.css to the class .prefooter .container-fluid [approximately row 154] -->
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h3>Чтобы не пропустить скидку<br> Достаточно подписаться на рассылку</h3>
+                            <p>Введите ваш Email адрес на который вы хотите получать уникальные предложения</p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" placeholder="Ваш emai...">
+                                    <br>
+                                    <button type="button" class="btn btn-danger">Подписаться</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
--- index.php
+++ index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="ru">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="/images/logo.jpg" type="image/png">
-        <title>Студия дизайна   </title>
-        <!-- Bootstrap core CSS -->
-        <link href="bootstrap/css/theme.css" rel="stylesheet">
-        <!-- Custom styles for this template -->
-        <link href="assets/css/style.css" rel="stylesheet">
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700,100' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Raleway:300,700,900,500' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.7/typicons.min.css">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="assets/css/pushy.css">
-        <link rel="stylesheet" href="assets/css/masonry.css">
-        <link rel="stylesheet" href="assets/css/animate.css">
-        <link rel="stylesheet" href="assets/css/magnific-popup.css">
-        <link rel="stylesheet" href="assets/css/odometer-theme-default.css">
-        <script>
-        window.odometerOptions = {
-          selector: '.odometer',
-          format: '(,ddd)', // Change how digit groups are formatted, and how many digits are shown after the decimal point
-          duration: 13000, // Change how long the javascript expects the CSS animation to take
-          theme: 'default'
-        };
-        </script>
-    </head>
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/assets/base/Header.php"?>
     <body class="">
       <!-- Pushy Menu -->
       <nav class="pushy pushy-left">
@@ -255,71 +219,27 @@
                 </div>
             </div>
         </section>
-        
-        <section id="contact" class="prefooter wow fadeInUp" data-wow-delay="300ms">
+
+       
+        <section id="contact" class="prefooter wow fadeInUp"  data-wow-delay="300ms">
             <!-- change the image in style.css to the class .prefooter .container-fluid [approximately row 154] -->
-            <div class="container-fluid">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <h3>Чтобы не пропустить скидку<br> Достаточно подписаться на рассылку</h3>
-                            <p>Введите ваш Email адрес на который вы хотите получать уникальные предложения</p>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control" placeholder="Ваш emai...">
-                                    <br>
-                                    <button type="button" class="btn btn-danger">Подписаться</button>
+                <div class="container-fluid">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h3>Чтобы не пропустить скидку<br> Достаточно подписаться на рассылку</h3>
+                                <p>Введите ваш Email адрес на который вы хотите получать уникальные предложения</p>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input name="email" type="text" class="form-control" placeholder="Ваш emai...">
+                                        <br>
+                                        <input type="sumbit" class="btn btn-danger">Подписаться</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </section>
-        <footer>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h3>Digital Hills</h3>
-                        <p>© 2021 Студия дизайна <a target="_blank" href="mailto:shantsev@internet.ru ?subject=Письмо от посетителя сайта студия дизайна &Body=text">Написать нам</a></p>
-                    </div>
-                    <div class="col-md-4">
-                        <p class="text-right social">
-                            <i class="typcn typcn-social-facebook-circular"></i>
-                            <i class="typcn typcn-social-twitter-circular"></i>
-                            <i class="typcn typcn-social-tumbler-circular"></i>
-                            <i class="typcn typcn-social-github-circular"></i>
-                            <i class="typcn typcn-social-dribbble-circular"></i>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Bootstrap core JavaScript
-    ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="assets/js/jquery.min.js"></script>
-        <script src="bootstrap/js/bootstrap.min.js"></script>
-        <script src="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-scrollspy.js"></script>
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-        <script src="http://masonry.desandro.com/masonry.pkgd.js"></script>
-        <script src="assets/js/masonry.js"></script>
-        <script src="assets/js/pushy.min.js"></script>
-        <script src="assets/js/jquery.magnific-popup.min.js"></script>
-        <script src="assets/js/wow.min.js"></script>
-        <script src="assets/js/scripts.js"></script>
-        <script src="assets/js/odometer.js"></script>
-    </body>
-</html>
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-34344036-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+ <?php include $_SERVER["DOCUMENT_ROOT"] . "/assets/base/Footer.php" ?>
